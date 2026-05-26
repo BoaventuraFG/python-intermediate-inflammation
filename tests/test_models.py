34a585671ba589107f73d697572180c0a706fb44
Fabio Boaventura <f.gasparottoboaventura@tudelft.nl>
2026-05-26 14:22:41 +0200

    pep8 code improvements

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,37 +6,41 @@ import numpy.testing as npt
 
 from inflammation.models import daily_mean, daily_max, daily_min
 
+
 @pytest.mark.parametrize(
-        "test_input, test_result",
-        [
-            ([[0, 0], [0, 0], [0,0]], [0, 0]),
-            ([[1, 2], [3, 4], [5, 6]], [3, 4]),
-            (np.zeros((3, 5)), np.zeros(5)),
-        ]
+    "test_input, test_result",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+        (np.zeros((3, 5)), np.zeros(5)),
+    ],
 )
 def test_daily_mean(test_input, test_result):
     """Test that mean function works for both zeroes and integers"""
 
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
 def test_daily_mean_strings():
     """Test for TypeError when parsing strings."""
-    
+
     with pytest.raises(TypeError):
-        error_expected = daily_mean(["mean", "test"])    # noqa: F841
+        error_expected = daily_mean(["mean", "test"])  # noqa: F841
+
 
 def test_daily_max_string():
     """Test for TypeError when parsing strings."""
 
     with pytest.raises(TypeError):
-        error_expected = daily_max(["Helo", "There"])   # noqa: F841
+        error_expected = daily_max(["Helo", "There"])  # noqa: F841
+
 
 @pytest.mark.parametrize(
-        "test_input, test_result",
-        [
-            (np.array([[-1, 2], [3, -4], [5, 6]]), np.array([5, 6])),
-            (np.array([[-1, 2], [3, -4], [-5, -6]]), np.array([3, 2])),
-        ]
+    "test_input, test_result",
+    [
+        (np.array([[-1, 2], [3, -4], [5, 6]]), np.array([5, 6])),
+        (np.array([[-1, 2], [3, -4], [-5, -6]]), np.array([3, 2])),
+    ],
 )
 def test_daily_max(test_input, test_result):
     """Test that max function works for an array of positive and negative integers."""
@@ -44,12 +48,11 @@ def test_daily_max(test_input, test_result):
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_max(test_input), test_result)
 
+
 def test_daily_min():
     """Test that min function works for an array of positive and negative integers."""
 
-    test_input = np.array([[1, -2],
-                           [3, 4],
-                           [5, -6]])
+    test_input = np.array([[1, -2], [3, 4], [5, -6]])
     test_result = np.array([1, -6])
 
     # Need to use Numpy testing functions to compare arrays
